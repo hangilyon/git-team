6a8fb5e0f438075ae52074d18def0289c5302e83
jwlee0714 <jwlee0714@naver.com>
2021-08-26 18:17:09 +0900

    이진원 작성중

---
+++ src/Lee/TestLee.java
@@ -0,0 +1,9 @@
+package Lee;
+
+public class TestLee {
+	public void testleefunc() {
+		System.out.println("이진원 테스트 파일 작성 완료!");
+	}
+}
+
+
--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -3,12 +3,14 @@ package main;
 import java.util.Scanner;
 
 import HanGilYong.TestHan;
+import Lee.TestLee;
 
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int num;
 		TestHan han = new TestHan();
+		TestLee lee = new TestLee();
 		while(true) {
 			System.out.println("1. 한길용 2. 이진원 3. 진동수 4. 이창훈 5. 이동호");
 			num = input.nextInt();
@@ -16,7 +18,8 @@ public class MainClass {
 			case 1 : 
 				han.testhanfunc();
 				break;
-			case 2 : break;
+			case 2 : 	lee.testleefunc();
+				break;
 			case 3 : break;
 			case 4 : break;
 			case 5 : break;
